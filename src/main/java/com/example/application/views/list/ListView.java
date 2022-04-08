8c720b67816f12891861f9066c1cd62338bf0cbe
mateonidas <teus.polec.other@gmail.com>
2022-04-08 22:22:59 +0200

    add test classes

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -12,7 +12,10 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.context.annotation.Scope;
 
+@org.springframework.stereotype.Component
+@Scope("prototype")
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("Contacts | Vaadin CRM")
 public class ListView extends VerticalLayout {
